dacac14c6d013f4347f12ed8f6265efd075440a8
hiteshshetty-dev <hitesh.shetty@contentstack.com>
2025-09-06 21:51:22 +0530

    chore: update @faker-js/faker to version 10.0.0 (tree-shaking support) and adds Loader for Huge Document page

--- site/pages/examples/huge-document.tsx
+++ site/pages/examples/huge-document.tsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
+import { css } from '@emotion/css'
+
+const loaderContainer = css`
+  flex-direction: column;
+  min-height: 80vh;
+`
+
+const spinner = css`
+  width: 50px;
+  height: 50px;
+  border: 3px solid rgba(0, 0, 0, 0.1);
+  border-top: 3px solid #667eea;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+  margin-bottom: 20px;
+`
+
+const title = css`
+  margin: 0;
+  font-size: 24px;
+  font-weight: 300;
+  letter-spacing: 0.5px;
+`
+
+const subtitle = css`
+  margin: 8px 0 0 0;
+  font-size: 14px;
+  font-weight: 300;
+`
+
+const Loader = () => (
+  <div className={`${loaderContainer} flex-center`}>
+    <div className={spinner} />
+    <h2 className={title}>Loading Huge Document</h2>
+    <p className={subtitle}>Preparing thousands of nodes...</p>
+  </div>
+)
 
 const HugeDocument = dynamic(() => import('../../examples/ts/huge-document'), {
   ssr: false,
+  loading: () => <Loader />,
 })
 
 export default function HugeDocumentPage() {
